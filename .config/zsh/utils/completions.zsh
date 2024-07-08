4e7fbec9a33c3d03d343e7b29ff52fad84ef85b5
Daniel Tahko <daniel.tahko@hotmail.com>
2024-07-09 01:46:13 +0200

    updates

--- .config/zsh/utils/completions.zsh
+++ .config/zsh/utils/completions.zsh
@@ -15,22 +15,6 @@ export FZF_DEFAULT_OPTS=" \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8" 
 
-## Add tmux?? https://arc.net/l/quote/nzvwocrj
-# Remove .git folders
-#
-# --tmux [center|top|bottom|left|right][,SIZE[%]][,SIZE[%]]
-
-# fzf --tmux center         # Center, 50% width and height
-# fzf --tmux 80%            # Center, 80% width and height
-# fzf --tmux 100%,50%       # Center, 100% width and 50% height
-# fzf --tmux left,40%       # Left, 40% width
-# fzf --tmux left,40%,90%   # Left, 40% width, 90% height
-# fzf --tmux top,40%        # Top, 40% height
-# fzf --tmux bottom,80%,40% # Bottom, 80% height, 40% height
-
-# Open in tmux popup if on tmux, otherwise use --height mode
-# export FZF_DEFAULT_OPTS='--height 40% --tmux bottom,40% --layout reverse --border top'
-
 # Completion styling
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}'
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
--- install/create_directories.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create XDG Base Directory
-mkdir -p $HOME/.config
-mkdir -p $HOME/.local/share
-mkdir -p $HOME/.local/state
-mkdir -p $HOME/.cache
-
