fc8d9f1221e666012a5b2f78b6d929a9e94683cc
Daniel Tahko <daniel.tahko@hotmail.com>
2024-07-08 23:56:17 +0200

    adding zinit configuration

---
+++ .config/zinit/zinitrc.zsh
@@ -0,0 +1,35 @@
+# Zinit init environment
+ZINIT_HOME="${XDG_DATA_HOME}/zinit/zinit.git"
+
+# Install zinit if not exists
+if [ ! -d $ZINIT_HOME ]; then
+  mkdir -p "$(dirname $ZINIT_HOME)"
+  git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+fi
+
+source "${ZINIT_HOME}/zinit.zsh"
+
+# zsh plugins
+zinit ice wait lucid
+zinit light zdharma-continuum/fast-syntax-highlighting
+
+zinit ice wait lucid
+zinit light zsh-users/zsh-completions
+
+zinit ice wait lucid
+zinit light zsh-users/zsh-autosuggestions
+
+zinit ice wait lucid
+zinit light Aloxaf/fzf-tab
+
+zinit ice wait lucid
+zinit light MichaelAquilina/zsh-you-should-use
+
+zinit ice wait lucid
+zinit light Homebrew/homebrew-command-not-found
+
+# zsh snippets
+zinit ice wait lucid
+zinit snippet OMZP::git/git.plugin.zsh
+zinit ice wait lucid
+zinit snippet OMZP::docker/docker.plugin.zsh
